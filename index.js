c11f98c7db3fb890f0785e7bb74f63f7735fa54a
Akshay Kamble <77927918+gadgetfather@users.noreply.github.com>
2021-09-24 18:56:31 +0000

    added more questions

--- index.js
+++ index.js
@@ -29,16 +29,16 @@ else {
 }
 
 var questions = [{
-  question:"Who is Akshay's favorite superhero? ",
+  question:"Who is Akshay's favorite superhero? (*enter option not option number!) \n 1. Hulk \n 2. iron man \n 3. Batman\n  ",
   answer:"Batman"
 },{
-  question:"Where does Akshay live? ",
+  question:"Where does Akshay live? (*enter option not option number!) \n 1. Ratnagiri \n2. Mumbai \n 3. Nashik\n  ",
   answer:"Ratnagiri" 
 },{
-  question:"How old is Akshay? ",
+  question:"How old is Akshay? (*enter option not option number!) \n 1. 21 \n 2. 19 \n 3. 18\n ",
   answer:"21"
 }, {
-  question:"Which graphics card does Akshay have? (hint = put only number like 3080) ",
+  question:"Which graphics card does Akshay have? (hint = put only number like 3080) (*enter option not option number!) \n 1. 1060 \n 2. 2070 \n 3. 310\n  ",
   answer:"1060"},
   {
   question:"How tall is Akshay?(*enter option not option number!) \n 1. 5.5ft \n 2. 6.3ft \n 3. 6.1ft\n ",
@@ -47,7 +47,13 @@ var questions = [{
   answer:"octane"},
   {
   question:"what is Akshay's favorite sport?(*enter option not option number!) \n 1.Cricket  \n 2.Volleyball \n 3.Football\n ",
-  answer:"Football"}
+  answer:"Football"},
+  {
+  question:"what is Akshay's favorite sweet?(*enter option not option number!) \n 1.gulabjamun  \n 2.Rasgulla \n 3.Jalebi\n ",
+  answer:"Football"},
+  {
+  question:"who is Akshay's favorite haikyu character?(*enter option not option number!) \n 1.Daichi  \n 2.Kageyama \n 3.Tanaka\n ",
+  answer:"Gulabjamun"}
   ]
 
   for(i=0; i<questions.length;i++){
